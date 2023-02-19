8c15c5567527ef4bc4c9defd33b11a77dcb18bc3
Kim Junsoo <blue3197@gmail.com>
2023-02-19 21:56:04 +0900

    fix the test

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Computes a factorial of a given positive integer
 function factorial(n) {
-  // if (n < 0) {
-  //   throw new Error('Factorial of negative number is not defined');
-  // }
+  if (n < 0) {
+    throw new Error('Factorial of negative number is not defined');
+  }
 
   if (n === 0) {
     return 1;
